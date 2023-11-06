2a59c43b703f53950e304a7538ec79175940948f
Nabila <134200060@student.upnyk.ac.id>
2023-11-06 20:29:17 +0700

    menambahkan layout activity login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.finalproject_chilicare"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.finalproject_chilicare"
@@ -36,7 +36,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
