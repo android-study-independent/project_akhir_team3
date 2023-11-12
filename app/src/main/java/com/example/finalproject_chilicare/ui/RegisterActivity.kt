05b7e96e900a51ab13035409b40ec37c807fbe88
Adi <haryadiwibowo87@gmail.com>
2023-11-12 12:20:23 +0700

    register negatif flow

--- app/src/main/java/com/example/finalproject_chilicare/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/finalproject_chilicare/ui/RegisterActivity.kt
@@ -3,8 +3,8 @@ package com.example.finalproject_chilicare.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Email
 import android.util.Log
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,59 +13,154 @@ import com.example.finalproject_chilicare.data.api.Network
 import com.example.finalproject_chilicare.data.api.UserAPI
 import com.example.finalproject_chilicare.data.response.RegisterRequest
 import com.example.finalproject_chilicare.data.response.RegisterResponse
+import com.google.android.material.textfield.TextInputLayout
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class RegisterActivity : AppCompatActivity() {
 
-    lateinit var email: EditText
-    lateinit var namaPengguna : EditText
-    lateinit var password : EditText
-    lateinit var konfirmasiPassword : EditText
-    lateinit var btRegister : Button
-    lateinit var tvLoginDisini : TextView
-
+    lateinit var emailContainer: TextInputLayout
+    lateinit var inputEmailRegister: EditText
+    lateinit var usernameContainer: TextInputLayout
+    lateinit var inputUsernameRegister: EditText
+    lateinit var passwordContainer: TextInputLayout
+    lateinit var inputPasswordRegister: EditText
+    lateinit var confirmPasswordContainer: TextInputLayout
+    lateinit var inputConfirmPasswordRegister: EditText
+    lateinit var btnRegister: Button
+    lateinit var textToLogin: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        email = findViewById(R.id.etEmail)
-        namaPengguna = findViewById(R.id.etNamaPengguna)
-        password = findViewById(R.id.etPassword)
-        konfirmasiPassword = findViewById(R.id.etKonfirmasiPassword)
-        btRegister = findViewById(R.id.btDaftarSekarang)
-        tvLoginDisini = findViewById(R.id.tvLoginDisini)
+        inputEmailRegister = findViewById(R.id.textInputEmailRegister)
+        emailContainer = findViewById(R.id.emailContainer)
+        usernameContainer = findViewById(R.id.usernameContainer)
+        inputUsernameRegister = findViewById(R.id.textInputUsername)
+        passwordContainer = findViewById(R.id.passwordContainer)
+        inputPasswordRegister = findViewById(R.id.textInputPasswordRegister)
+        confirmPasswordContainer = findViewById(R.id.confirmPasswordContainer)
+        inputConfirmPasswordRegister = findViewById(R.id.textInputConfirmPassword)
 
-        tvLoginDisini.setOnClickListener { Intent(this,LoginActivity::class.java).also {
+        textToLogin = findViewById(R.id.textToLogin)
+
+        textToLogin.setOnClickListener { Intent(this,LoginActivity::class.java).also {
             startActivity(it)
         } }
 
+        checkEmail()
+        checkUsername()
+        checkPassword()
+        checkConfirmPassword()
         initAction()
     }
 
-    fun initAction() {
-        btRegister.setOnClickListener {
-            postUser()
+    private fun checkEmail() {
+        inputEmailRegister.setOnFocusChangeListener { _, hasFocus ->  
+            if(!hasFocus){
+                emailContainer.helperText = validEmail()
+
+            }
         }
     }
 
-    fun postUser() {
+    private fun validEmail(): String? {
+        val txtEmail = inputEmailRegister.text.toString()
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(txtEmail).matches()){
+            return "Invalid Email Address"
+        } else if (txtEmail.isEmpty()){
+            return "Email must be entry"
+        }
+        return null
+    }
+
+    private fun checkUsername() {
+        inputUsernameRegister.setOnFocusChangeListener { _, hasFocus ->
+            if(!hasFocus){
+                usernameContainer.helperText = validUsername()
+            }
+        }
+    }
+
+    private fun validUsername(): String? {
+        val txtUsername = inputUsernameRegister.text.toString()
+
+         if (txtUsername.isEmpty()){
+            return "Username must be entry"
+        }
+        return null
+    }
+
+    private fun checkPassword() {
+        inputPasswordRegister.setOnFocusChangeListener { _, hasFocus ->
+            if(!hasFocus){
+                passwordContainer.helperText = validPassword()
+            }
+        }
+    }
+
+    private fun validPassword(): String? {
+        val txtPassword = inputPasswordRegister.text.toString()
+
+        if (txtPassword.length < 8){
+            return "Minimum character 8"
+        }
+        if (!txtPassword.matches((".*[A-Z].*".toRegex()))){
+            return "Must 1 upper-case character"
+        }
+        if (!txtPassword.matches((".*[a-z].*".toRegex()))){
+            return "Must 1 lower-case character"
+        }
+        if (!txtPassword.matches((".*[@#-_^].*".toRegex()))){
+            return "Must 1 spesial character : @, #, -, _, ^"
+        }
+        return null
+    }
+
+    private fun checkConfirmPassword() {
+        inputConfirmPasswordRegister.setOnFocusChangeListener { _, hasFocus ->
+            if(!hasFocus){
+                confirmPasswordContainer.helperText = validConfirmPassword()
+            }
+        }
+    }
+
+    private fun validConfirmPassword(): String? {
+        val txtPassword = inputPasswordRegister.text.toString()
+        val txtConfirmPassword = inputConfirmPasswordRegister.text.toString()
+
+        if (txtConfirmPassword !=  txtPassword){
+            return "Your confirm password not same"
+        }
+        return null
+    }
+
+
+    fun initAction() {
+        val btnRegister = findViewById<Button>(R.id.buttonRegister)
+
+        btnRegister.setOnClickListener {
+            createNewUser()
+        }
+    }
+
+    fun createNewUser() {
+        inputEmailRegister = findViewById(R.id.textInputUsername)
+        inputEmailRegister= findViewById(R.id.textInputEmailRegister)
+        inputPasswordRegister = findViewById(R.id.textInputPasswordRegister)
+
         val registerReq = RegisterRequest()
+        registerReq.fullname = inputEmailRegister.text.toString()
+        registerReq.email = inputEmailRegister.text.toString()
+        registerReq.password = inputPasswordRegister.text.toString()
 
-        registerReq.fullname = namaPengguna.text.toString()
-        registerReq.email = email.text.toString()
-        registerReq.password = password.text.toString()
-
-        val retro = Network().getRetroClientInstance("http://195.35.32.179:8003/auth/").create(UserAPI::class.java)
+        val retro = Network().getRetroClientInstance("https://35b3-103-189-201-221.ngrok-free.app/auth/").create(UserAPI::class.java)
         retro.createUser(registerReq).enqueue(object : Callback<RegisterResponse>{
             override fun onResponse(call: Call<RegisterResponse>, response: Response<RegisterResponse>) {
                 val register = response.body()
 
-//                val textStatus = findViewById<TextView>(R.id.textStatus)
-//                val textMessage = findViewById<TextView>(R.id.textMessage)
-//                textStatus.text = register!!.status.toString()
-//                textMessage.text = register!!.message.toString()
             }
 
             override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
@@ -80,45 +175,4 @@ class RegisterActivity : AppCompatActivity() {
     }
 
 }
-
-//        btnRegister.setOnClickListener {
-//            val checkFullname = fullname.text.toString().trim()
-//            val checkEmail = email.text.toString().trim()
-//            val checkPassword = password.text.toString().trim()
-//            val checkConfirmPassword = confirmPassword.text.toString().trim()
-//
-//            if (checkFullname.isEmpty()){
-//                fullname.error = "Fullname required"
-//                fullname.requestFocus()
-//                return@setOnClickListener
-//            }
-//
-//            else if (checkEmail.isEmpty()){
-//                email.error = "Email required"
-//                email.requestFocus()
-//                return@setOnClickListener
-//            }
-//            else if (checkPassword.isEmpty()){
-//                password.error = "password required"
-//                password.requestFocus()
-//                return@setOnClickListener
-//            }
-//            else if (checkConfirmPassword.isEmpty()){
-//                confirmPassword.error = "confirmPassword required"
-//                confirmPassword.requestFocus()
-//                return@setOnClickListener
-//            }
-//
-//            Network.instance.register(fullname.toString(), email.toString(), password.toString())
-//                .enqueue(object : Callback<UserResponse>{
-//                    override fun onResponse(call: Call<UserResponse>, response: Response<UserResponse>) {
-//                        Toast.makeText(applicationContext, response.body()?.message, Toast.LENGTH_LONG).show()
-//                    }
-//
-//                    override fun onFailure(call: Call<UserResponse>, t: Throwable) {
-//                        Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG).show()
-//                    }
-//                })
-//            Log.d("Debug", "Register succes")
-//        }
 
