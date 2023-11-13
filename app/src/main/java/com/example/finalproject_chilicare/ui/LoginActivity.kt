b9c0cfd6439e4763bec08fd89eb8686d078c3879
Nabila <134200060@student.upnyk.ac.id>
2023-11-13 14:09:09 +0700

    perbaikan layout register

--- app/src/main/java/com/example/finalproject_chilicare/ui/LoginActivity.kt
+++ app/src/main/java/com/example/finalproject_chilicare/ui/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.finalproject_chilicare.ui
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -14,16 +15,15 @@ import com.example.finalproject_chilicare.data.api.Network
 import com.example.finalproject_chilicare.data.api.UserAPI
 import com.example.finalproject_chilicare.data.response.LoginRequest
 import com.example.finalproject_chilicare.data.response.LoginResponse
-import com.example.finalproject_chilicare.ui.HomeActivity
-import com.example.finalproject_chilicare.ui.RegisterActivity
+import com.google.android.material.textfield.TextInputLayout
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var email: EditText
-    lateinit var password: EditText
+    lateinit var loginEmail: EditText
+    lateinit var loginPassword: EditText
     lateinit var tvLupaPassword: TextView
     lateinit var btLogin: Button
     lateinit var tvDaftarDisini: TextView
@@ -33,8 +33,8 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        email = findViewById(R.id.etEmail)
-        password = findViewById(R.id.etPassword)
+        loginEmail = findViewById(R.id.etEmail)
+        loginPassword = findViewById(R.id.etPassword)
         tvLupaPassword = findViewById(R.id.tvLupaPassword)
         btLogin = findViewById(R.id.btMasuk)
         tvDaftarDisini = findViewById(R.id.tvDaftarDisini)
@@ -51,36 +51,103 @@ class LoginActivity : AppCompatActivity() {
                 startActivity(it)
             }
         }
+        checkEmail()
+        checkPassword()
+        initAction()
+    }
+    //INVALID LOGIN
+    private fun checkEmail() {
+        loginEmail.setOnFocusChangeListener { _, hasFocus ->
+            val textInputLayout = findViewById<TextInputLayout>(R.id.tilEmail)
+            if (!hasFocus) {
+                textInputLayout.helperText = invalidEmail()
+            }
+        }
+    }
+    private fun invalidEmail(): String? {
+        val txtEmail = loginEmail.text.toString()
 
-        btLogin.setOnClickListener {
-            doLogin(email.text.toString(), password.text.toString())
+        if(!Patterns.EMAIL_ADDRESS.matcher(txtEmail).matches()){
+            return "Email yang anda masukan salah"
+        } else if (txtEmail.isEmpty()){
+            return "Masukan Alamat Email"
+        }
+        return null
+    }
+    private fun checkPassword() {
+        loginPassword.setOnFocusChangeListener { _, hasFocus ->
+            val textInputLayout = findViewById<TextInputLayout>(R.id.tilPassword)
+            if (!hasFocus) {
+                textInputLayout.helperText = invalidPassword()
+            }
         }
     }
 
-    private fun doLogin(email: String, password: String) {
-        val loginReq = LoginRequest(requestEmail = email, requestPassword = password)
+    private fun invalidPassword(): String? {
+        val txtPassword = loginPassword.text.toString()
 
-        val retro = Network().getRetroClientInstance("http://195.35.32.179:8003/auth/")
-            .create(UserAPI::class.java)
+        if (txtPassword.length < 8){
+            return "Minimal 8 karakter"
+        }
+        if (!txtPassword.matches((".*[A-Z].*".toRegex()))){
+            return "Harus ada 1 karakter huruf besar"
+        }
+        if (!txtPassword.matches((".*[a-z].*".toRegex()))){
+            return "Harus ada 1 karakter huruf kecil"
+        }
+        if (!txtPassword.matches((".*[@#-_^].*".toRegex()))){
+            return "Harus ada karakter spesial : @, #, -, _, ^"
+        }
+        return null
+    }
+    fun initAction() {
+        val btLogin = findViewById<Button>(R.id.btMasuk)
 
-        retro.userLogin(loginReq).enqueue(object : Callback<LoginResponse> {
-            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
-                if (response.isSuccessful) {
-                    response.body()?.data?.token?.let { token ->
-                        if (token.isNotEmpty()) {
-                            saveLoginData(token)
-                            navigateToHome()
+        btLogin.setOnClickListener {
+            doLogin()
+        }
+    }
+
+    private fun doLogin() {
+        val enteredEmail = loginEmail.text.toString()
+        val enteredPassword = loginPassword.text.toString()
+
+        val errorEmail = invalidEmail()
+        val errorPassword = invalidPassword()
+
+        if (errorEmail == null && errorPassword == null) {
+            val loginReq = LoginRequest(requestEmail = enteredEmail, requestPassword = enteredPassword)
+
+            val retro = Network().getRetroClientInstance("http://195.35.32.179:8003/auth/")
+                .create(UserAPI::class.java)
+
+            retro.userLogin(loginReq).enqueue(object : Callback<LoginResponse> {
+                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+                    if (response.isSuccessful) {
+                        response.body()?.data?.token?.let { token ->
+                            if (token.isNotEmpty()) {
+                                saveLoginData(token)
+                                navigateToHome()
+                            }
                         }
+                    } else {
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "Email atau password salah",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
-                } else {
-                    Toast.makeText(this@LoginActivity, "Email atau password salah", Toast.LENGTH_SHORT).show()
                 }
-            }
 
-            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                Toast.makeText(this@LoginActivity, "Terjadi kesalahan", Toast.LENGTH_SHORT).show()
-            }
-        })
+                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                    Toast.makeText(this@LoginActivity, "Terjadi kesalahan", Toast.LENGTH_SHORT).show()
+                }
+            })
+        } else {
+            // Display error messages for email and password
+            loginEmail.error = errorEmail
+            loginPassword.error = errorPassword
+        }
     }
 
     private fun saveLoginData(token: String) {
