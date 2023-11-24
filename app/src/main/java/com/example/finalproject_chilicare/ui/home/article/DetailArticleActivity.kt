810c9a2b6a587af182aeee531b602e92ab167940
Nabila <134200060@student.upnyk.ac.id>
2023-11-24 18:43:29 +0700

    memperbaiki error

--- app/src/main/java/com/example/finalproject_chilicare/ui/home/article/DetailArticleActivity.kt
+++ app/src/main/java/com/example/finalproject_chilicare/ui/home/article/DetailArticleActivity.kt
@@ -14,38 +14,38 @@ class DetailArticleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_article)
-        val cardClass: CardClass? = intent.getParcelableExtra<CardClass>("DataDetailArticle")
-        if (cardClass != null) {
-            val dataImage: ImageView = findViewById(R.id.ivArticle)
-            val txtTitle: TextView = findViewById(R.id.tvJudulartikel)
-            val txtDetailArticle: TextView = findViewById(R.id.tvDecArticle)
-            val tvTab: TextView = findViewById(R.id.tvTab)
+        //val cardClass: CardClass? = intent.getParcelableExtra<CardClass>("DataDetailArticle")
+        //if (cardClass != null) {
+        //val dataImage: ImageView = findViewById(R.id.ivArticle)
+        //val txtTitle: TextView = findViewById(R.id.tvJudulartikel)
+        //val txtDetailArticle: TextView = findViewById(R.id.tvDecArticle)
+        //val tvTab: TextView = findViewById(R.id.tvTab)
 
-            txtTitle.text = cardClass.dataSubtitle
-            txtDetailArticle.text = cardClass.dataDescription
-            tvTab.text = cardClass.dataTitle
-            dataImage.setImageResource(cardClass.dataImage)
-        }
-        override fun onBindViewHolder(holder: CardAdapter.CardViewHolder, position: Int) {
-            val currentItem = filteredList[position]
+        //txtTitle.text = cardClass.dataSubtitle
+        //txtDetailArticle.text = cardClass.dataDescription
+        //tvTab.text = cardClass.dataTitle
+        //dataImage.setImageResource(cardClass.dataImage)
+        //}
+        //override fun onBindViewHolder(holder: CardAdapter.CardViewHolder, position: Int) {
+        //  val currentItem = filteredList[position]
 
-            holder.rvTitle.text = currentItem.dataTitle
-            holder.rvSubtitle.text = currentItem.dataSubtitle
-            holder.rvDescription.text = currentItem.dataDescription
-            holder.rvImage.setImageResource(currentItem.dataImage)
+        //holder.rvTitle.text = currentItem.dataTitle
+        //holder.rvSubtitle.text = currentItem.dataSubtitle
+        //holder.rvDescription.text = currentItem.dataDescription
+        //holder.rvImage.setImageResource(currentItem.dataImage)
 
-            holder.itemView.setOnClickListener {
-                val intent = Intent(holder.itemView.context, DetailArticleActivity::class.java)
-                intent.putExtra("DataDetailArticle", currentItem)
-                holder.itemView.context.startActivity(intent)
-            }
-            val backButton: ImageView = findViewById(R.id.ivKembali)
-            backButton.setOnClickListener {
-                val intent = Intent(this, ArticleActivity::class.java)
-                startActivity(intent)
-                finish()
-            }
-        }
+        //holder.itemView.setOnClickListener {
+        //      val intent = Intent(holder.itemView.context, DetailArticleActivity::class.java)
+        //        intent.putExtra("DataDetailArticle", currentItem)
+        //        holder.itemView.context.startActivity(intent)
+        //    }
+        //   val backButton: ImageView = findViewById(R.id.ivKembali)
+        //  backButton.setOnClickListener {
+        //      val intent = Intent(this, ArticleActivity::class.java)
+        //      startActivity(intent)
+        //      finish()
+        //  }
+        //}
     }
 }
 
