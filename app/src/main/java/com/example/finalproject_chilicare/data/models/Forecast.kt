41d5ab64fc61673cde9c56bd4bc194f2edbda3d1
Adi <haryadiwibowo87@gmail.com>
2023-12-04 23:12:21 +0700

    resolved layouting weather

--- app/src/main/java/com/example/finalproject_chilicare/data/models/Forecast.kt
+++ app/src/main/java/com/example/finalproject_chilicare/data/models/Forecast.kt
@@ -3,5 +3,6 @@ package com.example.finalproject_chilicare.data.models
 data class Forecast(
     val date: String,
     val temperature: Double,
-    val weatherDescription: String
+    val weatherDescription: String,
+    val icon : String
 )
