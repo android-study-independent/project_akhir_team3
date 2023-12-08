10d8be433f85bc6b105cfd30e35e0c90e70ee75f
Adi <haryadiwibowo87@gmail.com>
2023-12-09 03:24:12 +0700

    berhasil mengambil data menggunakan parameter di header

--- app/src/main/java/com/example/finalproject_chilicare/data/models/AllForumItem.kt
+++ app/src/main/java/com/example/finalproject_chilicare/data/models/AllForumItem.kt
@@ -3,16 +3,34 @@ package com.example.finalproject_chilicare.data.models
 import com.google.gson.annotations.SerializedName
 
 data class AllForumItem(
+    @SerializedName ("captions")
     val captions: String,
+
+    @SerializedName ("createdAt")
     val createdAt: String,
+
+    @SerializedName ("forumId")
     val forumId: Int,
 
     @SerializedName ("id_user")
     val idUser: Int,
-    val image: List<String>,
+
+    @SerializedName ("image")
+    val image: List<ImageForum>,
+
+    @SerializedName ("jumlahKomentar")
     val jumlahKomentar: Int,
+
+    @SerializedName ("jumlahLike")
     val jumlahLike: Int,
 
     @SerializedName("name_user")
     val nameUser: String
 )
+{
+    data class ImageForum(
+        @SerializedName("image")
+        val image: String,
+    )
+}
+
--- app/src/main/java/com/example/finalproject_chilicare/data/models/AllForumResponse.kt
+++ app/src/main/java/com/example/finalproject_chilicare/data/models/AllForumResponse.kt
@@ -1,12 +1,18 @@
 package com.example.finalproject_chilicare.data.models
 
+import com.google.gson.annotations.SerializedName
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 data class AllForumResponse (
+    @SerializedName("status")
     val status : String,
+
+    @SerializedName("message")
     val message : String,
+
+    @SerializedName("data")
     val allForumItem : List<AllForumItem>
 
 )
