eaf1a23b148b3cee74d821a04dab93e4065754fa
Nabila <134200060@student.upnyk.ac.id>
2023-12-08 21:34:52 +0700

    Memperbaiki layouting favorite

--- app/src/main/java/com/example/finalproject_chilicare/ui/favorite/fragment/FavoriteFragment.kt
+++ app/src/main/java/com/example/finalproject_chilicare/ui/favorite/fragment/FavoriteFragment.kt
@@ -1,11 +1,14 @@
 package com.example.finalproject_chilicare.ui.favorite.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.finalproject_chilicare.R
+import com.example.finalproject_chilicare.ui.home.HomeActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +25,8 @@ class FavoriteFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var buttonKembali: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,8 +39,19 @@ class FavoriteFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        val view = inflater.inflate(R.layout.fragment_favorite, container, false)
+
+        view?.let { nonNullableView ->
+            buttonKembali = nonNullableView.findViewById(R.id.ivBack)
+
+            // Card ke activity Notification
+            buttonKembali.setOnClickListener {
+                val intent = Intent(requireContext(), HomeActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        return view
     }
 
     companion object {
