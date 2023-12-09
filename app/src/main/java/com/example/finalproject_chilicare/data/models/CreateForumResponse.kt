51dfe2d8f90fd1d8987979e7c2e177310dab3fe6
Adi <haryadiwibowo87@gmail.com>
2023-12-09 13:49:59 +0700

    resolved conflict

--- app/src/main/java/com/example/finalproject_chilicare/data/models/CreateForumResponse.kt
+++ app/src/main/java/com/example/finalproject_chilicare/data/models/CreateForumResponse.kt
@@ -1,7 +1,14 @@
 package com.example.finalproject_chilicare.data.models
 
+import com.google.gson.annotations.SerializedName
+
 data class CreateForumResponse(
+    @SerializedName("error")
     val error: Boolean,
+
+    @SerializedName("forum")
     val forum: AddNewForumResponse,
+
+    @SerializedName("message")
     val message: String
 )
